d63c1629051892d2640a940ad339dfd3ae038796
Kang Bongjo <bong5176@gmail.com>
2024-06-22 20:02:31 +0900

    기본 컴포넌트 작성

--- src/app/[...item]/page.tsx
+++ src/app/[...item]/page.tsx
@@ -1,7 +1,16 @@
-const ItemPage = ({ params: { item } }: { params: { item: string[] } }) => {
-  const path = ["", ...item].join("/");
+"use client";
 
-  return <div>Item Page</div>;
+import { ROUTE_PATH, isParentRoute, routes } from "@/routes";
+
+const ItemPage = ({ params: { item } }: { params: { item: string[] } }) => {
+  const path = ["", ...item].join("/") as ROUTE_PATH;
+  const route = routes[path];
+
+  if (!route || isParentRoute(route)) return null;
+
+  const { children: Component } = route;
+
+  return Component ? <Component /> : null;
 };
 
 export default ItemPage;
